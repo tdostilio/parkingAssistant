75b60d7bdd0a0f0c78ee9929480310c44cf6b96c
tdostilio <tdostilio@gmail.com>
2018-01-27 10:31:50 -0500

    adds license and apartment values

---
+++ formScript.js
@@ -0,0 +1,8 @@
+//License Plate 
+var license = document.getElementsByName('vehicle')[0];
+license.value = "ABCD"
+
+//Apartment
+var apartment = document.getElementsByName('tenant')[0];
+apartment.value = 5212
+
