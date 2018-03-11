5232c11f42c4c02733b044a3a638bd5cc5703dfb
Tom Dostilio <Tom@moquality.com>
2018-03-11 01:34:15 -0500

    adds renderPerson creator function

--- parking-app/App.js
+++ parking-app/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text, Card, ListItem, Button, CheckBox } from 'react-native-elements'
+
+
+
 
 export default class App extends React.Component {
+
+
+  renderPerson = (personArray) => {
+    return personArray.map((x, i) => {
+      return (
+        <View style={styles.personContainer}>
+          <CheckBox 
+            title={x.name}
+            checked={this.state.checked[i]}
+          />
+        </View>
+      )
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text h2>Parcheggio</Text>
+        <Text>Who are you registering?</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
@@ -18,6 +37,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: '35%'
   },
 });
