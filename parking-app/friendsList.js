66fe2a3c8baa384d5a5d40c2ff49b53e6742acfa
Tom Dostilio <tdostilio@gmail.com>
2018-03-27 19:37:33 -0400

    add friends

---
+++ parking-app/friendsList.js
@@ -0,0 +1,17 @@
+
+const friends = [
+  {"name": "Moose",
+  "license": "QAU9914"},
+  {"name": "Tia",
+  "license": "RKX0097"},
+  {"name": "Henri",
+  "license": "CGJ8734"},
+  {"name": "Joe",
+  "license": "CBC5449"},
+  {"name": "Ruderman",
+  "license": "CFH5491"},
+  {"name": "Brian",
+  "license": "CDK6646"},
+]
+
+export default friends
